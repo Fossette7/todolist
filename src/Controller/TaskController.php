89b8f78e53ea03305b685d638a83af62606757c7
Reybeka Peyrat <reybekaPeyrat@gmail.com>
2022-12-29 00:35:21 +0100

    Check role on update user and task + fix CSS

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -13,102 +13,109 @@ use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends AbstractController
 {
-    /**
-     * @Route("/tasks", name="task_list")
-     */
-    public function listAction(EntityManagerInterface $em)
-    {
-        return $this->render('task/list.html.twig', ['tasks' => $em->getRepository(Task::class)->findBy(['isDone' => 0])]);
+  /**
+   * @Route("/tasks", name="task_list")
+   */
+  public function listAction(EntityManagerInterface $em)
+  {
+    return $this->render('task/list.html.twig', ['tasks' => $em->getRepository(Task::class)->findBy(['isDone' => 0])]);
+  }
+
+  /**
+   * @Route("/tasks/done", name="task_list_done")
+   */
+  public function listTaskDoneAction(EntityManagerInterface $em)
+  {
+    $taskDone = $em->getRepository(Task::class)->findBy(['isDone'=> 1]);
+
+    return $this->render('task/list.html.twig', ['tasks' => $taskDone]);
+  }
+
+  /**
+   * @Route("/tasks/create", name="task_create")
+   */
+  public function createAction(Request $request, EntityManagerInterface $em)
+  {
+    $task = new Task();
+    $form = $this->createForm(TaskType::class, $task);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $task->setAuthor($this->getUser());
+
+      $em->persist($task);
+      $em->flush();
+
+      $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+      return $this->redirectToRoute('task_list');
     }
 
-    /**
-     * @Route("/tasks/done", name="task_list_done")
-     */
-    public function listTaskDone(EntityManagerInterface $em)
-    {
-      $taskDone = $em->getRepository(Task::class)->findBy(['isDone'=> 1]);
+    return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+  }
 
-      return $this->render('task/list.html.twig', ['tasks' => $taskDone]);
-    }
+  /**
+   * @Route("/tasks/{id}/edit", name="task_edit")
+   */
+  public function editAction(Task $task, Request $request, EntityManagerInterface $em, \Symfony\Component\Security\Core\Security $security)
+  {
+    if (
+      ($this->getUser() && $this->getUser() === $task->getAuthor()) ||
+      $security->isGranted('ROLE_ADMIN')
+    ) {
+      $form = $this->createForm(TaskType::class, $task);
 
-    /**
-     * @Route("/tasks/create", name="task_create")
-     */
-    public function createAction(Request $request, EntityManagerInterface $em)
-    {
-        $task = new Task();
-        $form = $this->createForm(TaskType::class, $task);
+      $form->handleRequest($request);
 
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $task->setAuthor($this->getUser());
-
-            $em->persist($task);
-            $em->flush();
-
-            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
-
-            return $this->redirectToRoute('task_list');
-        }
-
-        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
-    }
-
-    /**
-     * @Route("/tasks/{id}/edit", name="task_edit")
-     */
-    public function editAction(Task $task, Request $request, EntityManagerInterface $em)
-    {
-        $form = $this->createForm(TaskType::class, $task);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->flush();
-            $this->addFlash('success', 'La tâche a bien été modifiée.');
-
-            return $this->redirectToRoute('task_list');
-        }
-
-        return $this->render('task/edit.html.twig', [
-            'form' => $form->createView(),
-            'task' => $task,
-        ]);
-    }
-
-    /**
-     * @Route("/tasks/{id}/toggle", name="task_toggle")
-     */
-    public function toggleTaskAction(Task $task, EntityManagerInterface $em)
-    {
-        $task->toggle(!$task->isDone());
+      if ($form->isSubmitted() && $form->isValid()) {
         $em->flush();
-
-        $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        $this->addFlash('success', 'La tâche a bien été modifiée.');
 
         return $this->redirectToRoute('task_list');
-    }
+      }
 
-    /**
-     * @Route("/tasks/{id}/delete", name="task_delete")
-     * @param EntityManagerInterface $em
-     * @param Security $security
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function deleteTaskAction(Task $task, EntityManagerInterface $em, \Symfony\Component\Security\Core\Security $security) :RedirectResponse
+      return $this->render('task/edit.html.twig', [
+        'form' => $form->createView(),
+        'task' => $task,
+      ]);
+    } else {
+      return $this->redirectToRoute('task_list');
+    }
+  }
+
+  /**
+   * @Route("/tasks/{id}/toggle", name="task_toggle")
+   */
+  public function toggleTaskAction(Task $task, EntityManagerInterface $em)
+  {
+    $task->toggle(!$task->isDone());
+    $em->flush();
+
+    $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+
+    return $this->redirectToRoute('task_list');
+  }
+
+  /**
+   * @Route("/tasks/{id}/delete", name="task_delete")
+   * @param EntityManagerInterface $em
+   * @param Security $security
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   */
+  public function deleteTaskAction(Task $task, EntityManagerInterface $em, \Symfony\Component\Security\Core\Security $security) :RedirectResponse
+  {
+    if (
+      ($this->getUser() && $this->getUser() === $task->getAuthor()) ||
+      ($task->getAuthor() === null && $security->isGranted('ROLE_ADMIN'))
+    )
     {
-      if (
-            ($this->getUser() && $this->getUser() === $task->getAuthor()) ||
-            ($task->getAuthor() === null && $security->isGranted('ROLE_ADMIN'))
-          )
-        {
-          $em->remove($task);
-          $em->flush();
-          $this->addFlash('success', 'La tâche a bien été supprimée.');
-        }
-
-        return $this->redirectToRoute('task_list');
+      $em->remove($task);
+      $em->flush();
+      $this->addFlash('success', 'La tâche a bien été supprimée.');
     }
+
+    return $this->redirectToRoute('task_list');
+  }
 }
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -11,124 +11,116 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Task
 {
-    /**
-     * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+  /**
+   * @ORM\Column(type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
+  /**
+   * @ORM\Column(type="datetime")
+   */
+  private $createdAt;
 
-    /**
-     * @ORM\Column(type="string")
-     * @Assert\NotBlank(message="Vous devez saisir un titre.")
-     */
-    private $title;
+  /**
+   * @ORM\Column(type="string")
+   * @Assert\NotBlank(message="Vous devez saisir un titre.")
+   */
+  private $title;
 
-    /**
-     * @ORM\Column(type="text")
-     * @Assert\NotBlank(message="Vous devez saisir du contenu.")
-     */
-    private $content;
+  /**
+   * @ORM\Column(type="text")
+   * @Assert\NotBlank(message="Vous devez saisir du contenu.")
+   */
+  private $content;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isDone;
+  /**
+   * @ORM\Column(type="boolean")
+   */
+  private $isDone;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tasks")
-     */
-    private $Author;
+  /**
+   * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tasks")
+   */
+  private $Author;
 
-    public function __construct()
-    {
-        $this->createdAt = new \Datetime();
-        $this->isDone = false;
-    }
+  public function __construct()
+  {
+    $this->createdAt = new \Datetime();
+    $this->isDone = false;
+  }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    /**
-     * @param mixed $id
-     */
-    public function setId($id): void
-    {
-      $this->id = $id;
-    }
+  public function getCreatedAt() :\DateTime
+  {
+    return $this->createdAt;
+  }
 
-    public function getCreatedAt() :\DateTime
-    {
-        return $this->createdAt;
-    }
+  public function setCreatedAt($createdAt): void
+  {
+    $this->createdAt = $createdAt;
+  }
 
-    public function setCreatedAt($createdAt): void
-    {
-        $this->createdAt = $createdAt;
-    }
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+  public function setTitle(string $title): void
+  {
+    $this->title = $title;
+  }
 
-    public function setTitle(string $title): void
-    {
-        $this->title = $title;
-    }
+  public function getContent(): ?string
+  {
+    return $this->content;
+  }
 
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
+  /**
+   * @return false
+   */
+  public function getIsDone(): bool
+  {
+    return $this->isDone;
+  }
 
-    /**
-     * @return false
-     */
-    public function getIsDone(): bool
-    {
-      return $this->isDone;
-    }
+  /**
+   * @param false $isDone
+   */
+  public function setIsDone(bool $isDone): void
+  {
+    $this->isDone = $isDone;
+  }
 
-    /**
-     * @param false $isDone
-     */
-    public function setIsDone(bool $isDone): void
-    {
-      $this->isDone = $isDone;
-    }
+  public function setContent(string $content): void
+  {
+    $this->content = $content;
+  }
 
-    public function setContent(string $content): void
-    {
-        $this->content = $content;
-    }
+  public function isDone(): bool
+  {
+    return $this->isDone;
+  }
 
-    public function isDone(): bool
-    {
-        return $this->isDone;
-    }
+  public function toggle($flag): void
+  {
+    $this->isDone = $flag;
+  }
 
-    public function toggle($flag): void
-    {
-        $this->isDone = $flag;
-    }
+  public function getAuthor(): ?User
+  {
+    return $this->Author;
+  }
 
-    public function getAuthor(): ?User
-    {
-        return $this->Author;
-    }
+  public function setAuthor(?User $Author): self
+  {
+    $this->Author = $Author;
 
-    public function setAuthor(?User $Author): self
-    {
-        $this->Author = $Author;
-
-        return $this;
-    }
+    return $this;
+  }
 }
